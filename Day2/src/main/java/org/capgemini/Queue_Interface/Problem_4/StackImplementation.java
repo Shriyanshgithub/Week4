843f47685ee42adb3b42572578da122067b49162
Shriyanshgithub <gshriyansh703@gmail.com>
2025-02-12 15:57:05 +0530

    [Shriyansh Gupta] Add. Queue Interface Problem_4

--- Day2/src/main/java/org/capgemini/Queue_Interface/Problem_4/StackImplementation.java
+++ Day2/src/main/java/org/capgemini/Queue_Interface/Problem_4/StackImplementation.java
@@ -1,4 +1,22 @@
 package org.capgemini.Queue_Interface.Problem_4;
 
 public class StackImplementation {
+    public static void main(String[] args) {
+        // Create a stack and perform operations
+        StackUsingTwoQueue stack = new StackUsingTwoQueue();
+
+        // Push elements onto the stack
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        // Pop and print the top element
+        System.out.println("Popped element: " + stack.pop());
+
+        // Get and print the top element
+        System.out.println("Top element: " + stack.top());
+
+
+    }
 }
+
