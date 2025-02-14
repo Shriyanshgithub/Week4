a83bb7fa0ee26465446ff8f4046ee4a8edcdb9ca
Shriyanshgithub <gshriyansh703@gmail.com>
2025-02-14 14:51:51 +0530

    [Shriyansh Gupta] Add. Unchecked Exception (Runtime Exception)

---
+++ Day4/src/main/java/org/capgemini/UncheckedException/UncheckedException.java
@@ -0,0 +1,29 @@
+package org.capgemini.UncheckedException;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class UncheckedException {
+
+        public static void main(String[] args) {
+            try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
+                try{
+                    //takes two inputs and reads them
+                    System.out.print("Give input1: ");
+                    int a = Integer.parseInt(reader.readLine());
+                    System.out.print("Give input2: ");
+                    int b = Integer.parseInt(reader.readLine());
+                    //throws inner exception
+                    System.out.println(a/b);
+                }catch (ArithmeticException | NumberFormatException e){
+                    System.out.println(e.getMessage());
+                }
+            }catch (IOException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+
