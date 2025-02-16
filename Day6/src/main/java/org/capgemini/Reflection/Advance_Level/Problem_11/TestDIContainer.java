aad894e774907594db16ebf4105bd6fa8719d5f6
Shriyansh Gupta <gshriansh703@gmail.com>
2025-02-16 22:24:43 +0530

    [Shriyansh Gupta] Add. Reflection-Dependency Injection using Reflection

---
+++ Day6/src/main/java/org/capgemini/Reflection/Advance_Level/Problem_11/TestDIContainer.java
@@ -0,0 +1,19 @@
+package org.capgemini.Reflection.Advance_Level.Problem_11;
+
+public class TestDIContainer {
+    public static void main(String[] args) throws Exception {
+        SimpleDIContainer container = new SimpleDIContainer();
+
+        // Register services in the DI container
+        container.register(ServiceA.class);
+        container.register(ServiceB.class);
+
+        // Create ClientClass instance and inject dependencies
+        ClientClass client = new ClientClass();
+        container.injectDependencies(client);
+
+        // Use the injected dependencies
+        client.performOperations();
+    }
+}
+
