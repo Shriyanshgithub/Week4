34a6fc1b935501b1b4d9056376ee2212377ea2f6
Shriyansh Gupta <gshriansh703@gmail.com>
2025-02-16 19:38:33 +0530

    [Shriyansh Gupta] Add. Reflection-Access Private Field

---
+++ Day6/src/main/java/org/capgemini/Reflection/Basic_Level/Problem_3/InvokePrivateMethod.java
@@ -0,0 +1,13 @@
+package org.capgemini.Reflection.Basic_Level.Problem_3;
+
+public class InvokePrivateMethod {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        Class<?> cl = new 
+    }
+}
+class Calculator{
+    private double multiply(int a , int b){
+        return a*b;
+    }
+}
